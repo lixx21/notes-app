d3170e7ee68e316063e963b25c709224c1bf4664
felix <felix@solveeducation.org>
2024-08-18 13:53:32 +0700

    add button

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,9 @@
 import { Container } from 'postcss'
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { Button } from "@mui/material";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
 const Home = () => {
 
@@ -21,19 +24,38 @@ const Home = () => {
       .catch((error)=> console.error("There was a problem: ", error))
   }, []);
 
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <div className="container">
         
-        <div className='pt-36 justify-center'>
+        <div className='pt-20 justify-center'>
             <h1 className='font-bold text-center uppercase text-xl lg:text-4xl'>🚀 Welcome to My Notes 🚀</h1>
-
+            
             <div className='pt-10 px-10 flex flex-wrap max-w-full gap-4'>
+              <Button  
+                variant="contained"
+                color="primary"
+                size={isSmallScreen ? 'small' : 'medium'}
+                sx={{
+                  fontSize: isSmallScreen ? '0.7rem' : '1rem', // Adjust font size
+                  padding: isSmallScreen ? '5px 10px' : '8px 16px', // Adjust padding
+                }}
+                >Add Notes</Button>
+
+                {/* Card */}
                 {notes.map((note)=>(
                   <div className='max-w-full border-4 rounded-xl p-4 w-full overflow-hidden text-ellipsis'>
-                      <h3 className='font-bold text-2xl lg:text-3xl'>{note.title}</h3>
+                      <h3 className='font-bold text-xl lg:text-3xl'>{note.title}</h3>
                       <p className='pt-4 text-sm lg:text-lg pt-20 truncate'>{note.notes}</p>
                   </div>
                 ))}
+
+                  <div className='max-w-full border-4 rounded-xl p-4 w-full overflow-hidden text-ellipsis'>
+                      <h3 className='font-bold text-xl lg:text-3xl'>Title</h3>
+                      <p className='pt-4 text-sm lg:text-lg pt-20 truncate'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, velit reiciendis. Quasi illo fugit natus atque hic non itaque possimus aliquam, nostrum esse similique aut veniam expedita eligendi enim architecto!</p>
+                  </div>
             </div>
         </div>
 
